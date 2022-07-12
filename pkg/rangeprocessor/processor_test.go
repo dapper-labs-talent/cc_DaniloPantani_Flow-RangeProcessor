3af99d520118151b160f2b0fea158c27551f7f8a
Danilo Pantani <danpantani@gmail.com>
2022-07-11 22:16:50 -0300

    Add the `ProcessRange` extension method for the `rangeprocessor` and add the unit tests

--- pkg/rangeprocessor/processor_test.go
+++ pkg/rangeprocessor/processor_test.go
@@ -69,6 +69,203 @@ func TestRangeResponseProcessor_GetActiveRange(t *testing.T) {
 	}
 }
 
+func TestRangeResponseProcessor_ProcessRange(t *testing.T) {
+	t.Run("test new node sync", func(t *testing.T) {
+		r := New(3, 4, 0)
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(0), gotMinHeight)
+		require.Equal(t, uint64(2), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(3, []Block{"block_3", "block_4", "block_5"})
+		r.ProcessRange(3, []Block{"block_3", "block_4", "block_5"})
+		r.ProcessRange(3, []Block{"block_3", "block_4", "block_5"})
+		r.ProcessRange(3, []Block{"block_3", "block_4", "block_5"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(6), gotMinHeight)
+		require.Equal(t, uint64(8), gotMaxHeight)
+
+		r.ProcessRange(6, []Block{"block_6", "block_7", "block_8"})
+		r.ProcessRange(6, []Block{"block_6", "block_7", "block_8"})
+		r.ProcessRange(6, []Block{"block_6", "block_7", "block_8"})
+		r.ProcessRange(6, []Block{"block_6", "block_7", "block_8"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(9), gotMinHeight)
+		require.Equal(t, uint64(11), gotMaxHeight)
+	})
+
+	t.Run("test out of range height", func(t *testing.T) {
+		r := New(3, 2, 0)
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(0), gotMinHeight)
+		require.Equal(t, uint64(2), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_3", "block_4", "block_5"})
+		r.ProcessRange(0, []Block{"block_3", "block_4", "block_5"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(6, []Block{"block_3", "block_4", "block_5"})
+		r.ProcessRange(6, []Block{"block_3", "block_4", "block_5"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7"})
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(6), gotMinHeight)
+		require.Equal(t, uint64(8), gotMaxHeight)
+	})
+
+	t.Run("test with already existing blocks", func(t *testing.T) {
+		r := New(3, 3, 3)
+		r.blockCounter.Store(0, "block_0")
+		r.blockCounter.Store(1, "block_1")
+		r.blockCounter.Store(2, "block_2")
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(3, []Block{"block_3", "block_4"})
+		r.ProcessRange(3, []Block{"block_3", "block_4"})
+		r.ProcessRange(3, []Block{"block_3", "block_4"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(5), gotMinHeight)
+		require.Equal(t, uint64(7), gotMaxHeight)
+	})
+
+	t.Run("test block number greater than the range", func(t *testing.T) {
+		r := New(2, 4, 0)
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(0), gotMinHeight)
+		require.Equal(t, uint64(1), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(2), gotMinHeight)
+		require.Equal(t, uint64(3), gotMaxHeight)
+
+		r.ProcessRange(2, []Block{"block_5", "block_6", "block_7", "block_8", "block_9"})
+		r.ProcessRange(2, []Block{"block_5", "block_6", "block_7", "block_8", "block_9"})
+		r.ProcessRange(2, []Block{"block_5", "block_6", "block_7", "block_8", "block_9"})
+		r.ProcessRange(2, []Block{"block_5", "block_6", "block_7", "block_8", "block_9"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(4), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(4, []Block{"block_10", "block_11", "block_12", "block_13", "block_14"})
+		r.ProcessRange(4, []Block{"block_10", "block_11", "block_12", "block_13", "block_14"})
+		r.ProcessRange(4, []Block{"block_10", "block_11", "block_12", "block_13", "block_14"})
+		r.ProcessRange(4, []Block{"block_10", "block_11", "block_12", "block_13", "block_14"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(6), gotMinHeight)
+		require.Equal(t, uint64(7), gotMaxHeight)
+	})
+
+	t.Run("test nodes with less blocks", func(t *testing.T) {
+		r := New(3, 3, 0)
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(0), gotMinHeight)
+		require.Equal(t, uint64(2), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(3, []Block{"block_3"})
+		r.ProcessRange(3, []Block{"block_3"})
+		r.ProcessRange(3, []Block{"block_3"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(4), gotMinHeight)
+		require.Equal(t, uint64(6), gotMaxHeight)
+
+		r.ProcessRange(6, []Block{"block_4", "block_5"})
+		r.ProcessRange(6, []Block{"block_4", "block_5"})
+		r.ProcessRange(6, []Block{"block_4", "block_5"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(7), gotMinHeight)
+		require.Equal(t, uint64(9), gotMaxHeight)
+	})
+
+	t.Run("test nodes with messy blocks", func(t *testing.T) {
+		r := New(3, 4, 0)
+
+		gotMinHeight, gotMaxHeight := r.GetActiveRange()
+		require.Equal(t, uint64(0), gotMinHeight)
+		require.Equal(t, uint64(2), gotMaxHeight)
+
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4", "block_5"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2", "block_3", "block_4", "block_5", "block_6"})
+		r.ProcessRange(0, []Block{"block_0", "block_1", "block_2"})
+		r.ProcessRange(0, []Block{"block_0", "block_2"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(3), gotMinHeight)
+		require.Equal(t, uint64(5), gotMaxHeight)
+
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7", "block_8"})
+		r.ProcessRange(5, []Block{"block_5"})
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7", "block_8", "block_9", "block_10"})
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7", "block_8", "block_9"})
+		r.ProcessRange(5, []Block{"block_5", "block_6", "block_7"})
+		r.ProcessRange(5, []Block{"block_5", "block_6"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(6), gotMinHeight)
+		require.Equal(t, uint64(8), gotMaxHeight)
+
+		r.ProcessRange(8, []Block{"block_8", "block_9", "block_10", "block_11", "block_12"})
+		r.ProcessRange(8, []Block{"block_8"})
+		r.ProcessRange(8, []Block{"block_8", "block_9", "block_10"})
+		r.ProcessRange(8, []Block{"block_8", "block_9", "block_10", "block_11"})
+
+		gotMinHeight, gotMaxHeight = r.GetActiveRange()
+		require.Equal(t, uint64(9), gotMinHeight)
+		require.Equal(t, uint64(11), gotMaxHeight)
+	})
+}
+
 func TestBlock_Equals(t *testing.T) {
 	tests := []struct {
 		name string
